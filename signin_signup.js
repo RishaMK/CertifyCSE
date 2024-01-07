5b2727d3c101078f0c7efdb3540fde22216e2abb
Risha MK <rishamkeerthi@gmail.com>
2024-01-07 23:20:09 +0530

    js file created

---
+++ signin_signup.js
@@ -0,0 +1,19 @@
+function homePage(){
+    window.location.href="home_page.html";
+}
+
+
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+let form = document.getElementById("form");
+let endLine= document.getElementById("endLine");
+
+signinBtn.onclick = function(){
+    nameField.style.display="none";
+    form.style.maxHeight="500px";
+    title.innerText="Sign-In";
+    endLine.style.display="none";
+    return false;
+}
